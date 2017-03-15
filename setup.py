e9c21b792eb8f5ae82e63e1efd376e465fa08845
andresberejnoi <andresberejnoi@gmail.com>
2017-03-15 18:08:45 -0400

    Now using setuptools and also added information about dependencies

--- setup.py
+++ setup.py
@@ -5,19 +5,22 @@ Created on Wed Mar  8 17:12:11 2017
 @author: andresberejnoi
 """
 
-from distutils.core import setup
-desc = """Neural Network Builder
-The neural network class is in NeuralNet.py It allows to easily create fully connected feedforward networks of any size allowed by available memory. 
+from setuptools import setup
+long_desc = """Neural Network Builder
+The neural network class is in NeuralNet.py. It allows to easily create fully connected feedforward networks of any size allowed by available memory. 
 It uses numpy arrays as the primary data structure for the weight matrices. 
 With this class you can create deep neural networks very quickly (see some of the example files to see how to use it).
 """
-setup(name='NetBuilder',
+desc = """Allows to create and train fully connected feedforward deep neural networks in a simple way"""
+setup(name='netbuilder',
       version='0.1.0',
       packages=['NetBuilder'],
       author='Andres Berejnoi',
       author_email='andresberejnoi@gmail.com',
       url='https://github.com/andresberejnoi/NetBuilder',
       license='MIT',
+      description=desc,
       long_description=desc,
-      keywords=['Neural Network', 'machine learning', 'AI', 'artificial intelligence']
+      keywords=['Neural Network', 'machine learning', 'AI', 'artificial intelligence'],
+      install_requires=['numpy>=1']
       )

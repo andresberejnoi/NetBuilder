e02498990404ecb3f2b5e2551873b559c1f64bf2
Andres Berejnoi <andresberejnoi@gmail.com>
2017-03-19 04:32:10 -0400

    Now version number is read from version file __version__

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ Created on Wed Mar  8 17:12:11 2017
 
 from setuptools import setup, find_packages
 import os
+from NetBuilder import __version__
 
 def package_files(directory):
     paths = []
@@ -15,18 +16,16 @@ def package_files(directory):
             paths.append(os.path.join('..', path, filename))
     return paths
 
-paths = package_files('~/Github/NetBuilder/NetBuilder')
-
 long_desc = """Neural Network Builder
 The neural network class is in NeuralNet.py. It allows to easily create fully connected feedforward networks of any size allowed by available memory. 
 It uses numpy arrays as the primary data structure for the weight matrices. 
 With this class you can create deep neural networks very quickly (see some of the example files to see how to use it).
 """
-desc = """Allows to create and train fully connected feedforward deep neural networks in a simple way"""
+desc = """Allows to create and train fully connected feedforward deep neural networks in a simple way."""
 #with open('VERSION', 'r') as ver:
 #    version = ver.read().rstrip()
 setup(name='netbuilder',
-      version='0.1.2',
+      version=__version__,
       #packages=['NetBuilder'],
       packages=find_packages(),
       #package_data={'':paths},
@@ -40,5 +39,9 @@ setup(name='netbuilder',
       description=desc,
       long_description=long_desc,
       keywords=['Neural Network', 'machine learning', 'AI', 'artificial intelligence'],
-      install_requires=['numpy>=1']
+      install_requires=['numpy>=1'],
+      classifiers=['Programming Language :: Python',
+                   'Programming Language :: Python :: 3',
+                   'Development Status :: 3 - Alpha',
+                   'Operating System :: OS Independent']
       )

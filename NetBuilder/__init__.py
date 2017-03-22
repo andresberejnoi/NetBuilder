899cfea85219f08e15e89f5858559963bdac2503
Andres Berejnoi <andresberejnoi@gmail.com>
2017-03-22 16:44:01 -0400

    Files got deleted

--- NetBuilder/__init__.py
+++
@@ -1,6 +0,0 @@
-from NetBuilder.examples import *
-from NetBuilder.tests import *
-from .__version__ import __version__
-from .NeuralNet import *
-from .debug_test import *
-from .file_operations import *

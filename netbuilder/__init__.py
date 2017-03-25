562d42dfdfe2da74c6178f2a02b3199e3b76fd3e
Andres Berejnoi <andresberejnoi@gmail.com>
2017-03-25 04:17:00 -0400

    Importing _param_keys

--- netbuilder/__init__.py
+++ netbuilder/__init__.py
@@ -1,6 +1,7 @@
-from NetBuilder.examples import *
-from NetBuilder.tests import *
+#from . import examples
+#from . import tests
 from .__version__ import __version__
 from .NeuralNet import *
 from .debug_test import *
 from .file_operations import *
+import _param_keys

fa8d272ba5b2d2bdb6caac45aad1598383358c29
Andres Berejnoi <andresberejnoi@gmail.com>
2017-03-25 04:15:06 -0400

    A file to keep dictionary keys in one place

---
+++ netbuilder/_param_keys.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 25 03:54:17 2017
+
+@author: andres
+"""
+
+__weights_file = 'weights_file'
+__topology = 'topology'
+__size = 'size'
+__output_activation = 'output_activation'
+__hidden_activation = 'hidden_activation'
+__learning_rate = 'learning_rate'
+__momentum = 'momentum'
+__nane = 'name'
